61392e6ae2bba634a9923bef283c5e0e72b6a8d4
Brandon Lai-Cheong <brandon.lai-cheong@uwaterloo.ca>
2024-10-29 19:57:58 -0400

    added union tests

--- tests/union_tests.cpp
+++ tests/union_tests.cpp
@@ -1,6 +1,23 @@
 #include "gtest/gtest.h"
 #include <union.h>
+#include<triangle.h>
 
 TEST (UnionTrivialTests, NotTouching) {
+    Triangle t1 = Triangle{{0,0}, {5,0}, {2,3}, 0};
+    Triangle t2 = Triangle{{6,0}, {10,0}, {7,5}, 0};
 
+    auto ts = unionize(t1, t2);
+
+    EXPECT_TRUE(ts.size() == 2);
+}
+
+
+TEST (UnionTests, TwoIntersections) {
+
+    auto t1 = Triangle(Point{0,0}, Point{5,0},  Point{2,3}, 0);
+    auto t2 = Triangle(Point{3,1}, Point{6,1}, Point{4,3}, 0);
+
+    auto ts = unionize(t1, t2);
+
+    EXPECT_TRUE(ts.size() > 0);
 }
